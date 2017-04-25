a8546091242a30af96539da3e234b1247925da3a
yudaikaneko <yudaiabgc@gmail.com>
2017-04-25 07:21:17 +0000

    CRUD task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  
+ def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(message_params)
+    
+      if @task.save
+        flash[:success] = 'Taskが正常に投稿されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Taskが投稿されませんでした'
+      render :new
+      end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Taskは正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskは更新されませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Messageは正常に削除されました'
+    redirect_to message_url
+  end
+
+private
+  
+def message_params
+  params.require(:message).permit(:content)
+end
+end

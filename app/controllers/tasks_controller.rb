a13b5ffb3ef2ead74efc64378007462f05980919
yudaikaneko <yudaiabgc@gmail.com>
2017-04-26 08:06:44 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
+    @Task = Task.find(params[:id])
   end
   
   def new
@@ -25,11 +26,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -41,7 +40,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    set_message
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
@@ -49,8 +48,13 @@ class TasksController < ApplicationController
   end
 
 private
-  
+
+  def set_message
+    @task = Task.find(params[:id])
+  end
+
+
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426002634) do
+ActiveRecord::Schema.define(version: 20170426074726) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170426002634) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
